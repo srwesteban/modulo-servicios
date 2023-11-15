98a0e959f77f4b4bf908b89658562c6e35ef0046
William David Esteban Mora <81768394+srwesteban@users.noreply.github.com>
2023-11-15 16:53:40 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000; // Puedes configurar el puerto que desees
+const port = 3000; 
 
-// Middleware para parsear JSON en las solicitudes
 app.use(bodyParser.json());
 
-// Importa el módulo de CORS
 const cors = require('cors');
 
-// Configura CORS para permitir solicitudes desde el origen de tu aplicación React
 app.use(cors({
-  origin: 'http://localhost:4000', // Reemplaza con el origen de tu aplicación React
+  origin: 'http://localhost:4000', // Reemplaza con el origen de la aplicación React
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type',
 }));
 
-// Importa las rutas de los servicios
 const servicioRoutes = require('./src/app/routes/servicioRoutes');
 
-// Rutas de los servicios
 app.use('/servicios', servicioRoutes);
 
-// Inicia el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
